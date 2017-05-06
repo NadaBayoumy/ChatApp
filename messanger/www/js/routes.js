ffeeac561f5ee522524cdb5006cffdb88f240f06
NadaBayoumy <nada.bayoumy@hotmail.com>
2017-05-06 17:08:44 +0200

    adding templates

--- messanger/www/js/routes.js
+++ messanger/www/js/routes.js
@@ -1,31 +1,119 @@
+//js/routes.js
 angular.module('messanger').config(function($stateProvider) {
-    $stateProvider
-    .state('app',{
-        url : '/app',
-        templateUrl : "templates/app.html",
-        controller: 'app',
-        abstract: true
-    })
-    .state('app.about',{
-        url:'/about',
-        views:{
-          "pageContent":{
-            templateUrl:"templates/about.html"
-          }
-        }
-    })
-    .state('home',{
-        url : '',
-        templateUrl : "templates/home.html"
-    })
-    .state('register',{
-        url : '/register',
-        templateUrl : "templates/register.html",
-        controller: 'register'
-    })
-    .state('login',{
-        url : '/login',
-        templateUrl : "templates/login.html",
-        controller: 'login'
-    })
+  $stateProvider
+
+  .state('app',{
+    url:'/app',
+    templateUrl:"templates/app.html",
+    abstract:true,
+    // controller:"app"
+  })
+
+  // .state('app.home',{
+  //   url:'/home',
+  //   views:{
+  //     "pageContent":{
+  //       templateUrl:"templates/home.html"
+  //     }
+  //   }
+  // })
+
+  // .state('app.home',{
+  //   url:'/',
+  //   templateUrl:"templates/home.html",
+  // })
+
+
+  .state('home',{
+    url:"/",
+    templateUrl:"templates/home.html",
+  })
+
+
+  .state('login',{
+    url:'/login',
+    templateUrl:"templates/login.html",
+    // controller:"login"
+  })
+
+
+
+
+  .state('register',{
+    url:'/register',
+    templateUrl:"templates/register.html",
+    // controller:"signup"
+  })
+
+
+
+  .state('app.activeusers',{
+    url:'/activeusers',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/activeusers.html"
+      }
+    }
+  })
+
+
+
+
+
+  .state('app.about',{
+    url:'/about',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/about.html"
+      }
+    }
+  })
+
+  .state('app.settings',{
+    url:'/settings',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/settings.html"
+      }
+    }
+  })
+
+  .state('app.products',{
+    url:'/products',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/products.html",
+        controller:"products"
+      }
+    }
+  })
+
+
+
+   .state('app.product',{
+    url:'/product/:product_id',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/product.html",
+        controller:"product"
+      }
+    }
+  })
+
+
+
+  .state('app.singleproduct',{
+    url:'/product/singleproduct',
+    views:{
+      "pageContent":{
+        templateUrl:"templates/singleproduct.html",
+        controller:"product"
+      }
+    }
+  })
+
+
+
+
+
 })
