52522ee8f491dbcc111bf44d127dd111abc8bb2e
NadaBayoumy <nada.bayoumy@hotmail.com>
2017-05-08 18:51:57 +0200

    MMSN Mohamed, Mina, Simona And Nada Design except splash and logo

--- messanger/www/js/controllers/login.js
+++ messanger/www/js/controllers/login.js
@@ -6,9 +6,21 @@ angular.module('messanger').controller('login', function ($scope, User, $rootSco
         console.log($scope.saveduser);
         if (valid) {
             localStorage.setItem('username', $scope.saveduser.username)
+
+            // console.log('**GET INFO OF USER ',$scope.saveduser.username);
+            User.getuserinfo($scope.saveduser.username).then(function (res) {
+                console.log('inside get user info new api ' , res);
+                
+            }, function (err) {
+            })
+
+
+
+
             User.login($scope.saveduser).then(function (res) {
 
                 $rootScope.loggedinuser = $scope.saveduser;
+                console.log("rootscope of logged in" ,$rootScope.loggedinuser  );
                 if (res.status == 1) {
                     socket.open();
                     socket.emit('signin', $scope.saveduser.username, 'online');
--- messanger/www/js/controllers/register.js
+++ messanger/www/js/controllers/register.js
@@ -7,7 +7,22 @@ angular.module('messanger').controller('register',function($scope,User,$rootScop
             User.register($scope.user).then(function(res) {
 
             if(res.status == 1){
-                $state.go('app.activeUsers');
+                User.login($scope.user).then(function (res) {
+
+                    $rootScope.loggedinuser = $scope.user;
+                    console.log("rootscope of logged in" ,$rootScope.loggedinuser  );
+                    if (res.status == 1) {
+                        socket.open();
+                        socket.emit('signin', $scope.user.username, 'online');
+                        $state.go('app.activeUsers');
+                    } else {
+                        alert('not valid user')
+                    }
+
+                }, function (err) {
+
+                })
+                // $state.go('app.activeUsers');
             }else{
               alert('not valid user')
             }
